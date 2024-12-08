89fad9274c1be41a0f4239822ceae2d5f09556b5
yadnesh.salvi <yadnesh.salvi@contractpodai.com>
2024-12-08 21:01:54 +0530

    first commit

---
+++ agent.py
@@ -0,0 +1,11 @@
+# agent.py
+from langchain_anthropic import ChatAnthropic
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langgraph.prebuilt import create_react_agent
+
+model = ChatAnthropic(model="claude-3-5-sonnet-20240620")
+
+tools = [TavilySearchResults(max_results=2)]
+
+# compiled graph
+graph = create_react_agent(model, tools)
